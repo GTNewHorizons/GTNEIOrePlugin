aa56c64580122e7d8be64b694cf0532987124c97
miozune <miozune@gmail.com>
2023-06-18 13:04:50 +0900

    Redundant throws

--- src/main/java/pers/gwyog/gtneioreplugin/util/Veinrenamer.java
+++ src/main/java/pers/gwyog/gtneioreplugin/util/Veinrenamer.java
@@ -1,13 +1,11 @@
 package pers.gwyog.gtneioreplugin.util;
 
 import com.opencsv.bean.AbstractBeanField;
-import com.opencsv.exceptions.CsvConstraintViolationException;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
 
 public class Veinrenamer<T> extends AbstractBeanField<T> {
 
     @Override
-    protected Object convert(String value) throws CsvDataTypeMismatchException, CsvConstraintViolationException {
+    protected Object convert(String value) {
         String ret = null;
         CharSequence s = "/";
 
